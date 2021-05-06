ab42d1d2aea7f650e1b1e8c9fadb4517d9ebca31
protectsplash <db99136@naver.com>
2021-05-06 18:18:54 +0900

    feat: 데이터 적용!

--- src/views/search.vue
+++ src/views/search.vue
@@ -4,19 +4,59 @@
 			<v-row class="pt-16" style="position: relative">
 				<!--    리스트 결과 column      -->
 				<v-col class="px-12 py-16" sm="12" md="6">
-					<heading-4 class="font-weight-bold mt-2">제주도 식당 소개 </heading-4>
-					<v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-						<v-tab v-for="item in items" :key="item">
-							{{ item }}
-						</v-tab>
-					</v-tabs>
-
-					<v-tabs-items v-model="tab">
-						<v-tab-item v-for="item in items" :key="item">
-							<list></list>
+					<!--    타이틀 및 필터      -->
+					<v-row class="flex-column pa-0">
+						<heading-4 class="font-weight-bold mt-2">제주도 식당 소개</heading-4>
+					</v-row>
+					<!--    숙소 결과 리스트     -->
+					<v-row class="px-0 pb-3 pt-5" :class="$style.hotelContainer" v-for="item in 3" :key="item.recordid">
+						<v-col class="pa-0" cols="5">
+							<img
+								src="http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg"
+								class="rounded-xl"
+								alt
+								style="width: 100%; height: 100%"
+							/>
+							<!-- <v-sheet v-else class="rounded-xl" color="#cccccc" style="width: 100%; height: 100%" /> -->
+						</v-col>
+						<v-col class="pl-4 py-0" cols="7">
+							<div class="text-body-2 text--disabled" :style="{ width: '100%', position: 'relative' }">
+								<span>neighbourhood_cleansed 의 room_type</span>
+								<v-btn fab icon small :style="{ position: 'absolute', right: '-22px', top: '-7px' }">
+									<v-icon>favorite_border</v-icon>
+								</v-btn>
+							</div>
+							<div class="text-h6">name</div>
+							<v-divider class="my-2" width="30">
+								inset
+							</v-divider>
+							<div class="text-body-2" :class="$style.hotelDescription">
+								최대 인원 guests_included}명 &middot; 침실 bedrooms개 &middot; 침대 beds개 &middot; 욕실 bathrooms개
+							</div>
+							<div class="text-body-2 mt-1 mb-2" :class="$style.hotelDescription">
+								<!-- {{ splitString(item.fields.amenities, 0) }} &middot; {{ splitString(item.fields.amenities, 1) }} &middot;
+								{{ splitString(item.fields.amenities, 2) }} -->
+								여기에 뭐가 있음
+							</div>
+							<div class="mt-16" :style="{ position: 'relative', width: '100%' }">
+								<v-icon color="pink" size="19">star</v-icon>
+								<div>점수</div>
+								<div>후기</div>
+								<!-- <span class="text-body-2 font-weight-bold"> {{ convertRating(item.fields.review_scores_rating) }} </span> -->
+								<!-- <span class="text-body-2" :class="$style.hotelDescription">(후기 {{ item.fields.number_of_reviews }}개)</span> -->
+								<div :style="{ position: 'absolute', right: '-10px', top: '1px' }">
+									<!-- <span class="font-weight-bold text-body-1">$ {{ item.fields.price }} </span>/ 1박 -->
+									가격
+								</div>
+							</div>
+						</v-col>
+					</v-row>
+					<!--    페이지네이션      -->
+					<v-row class="pa-0 mt-16 flex-column" justify="center">
+						<div class="text-center">
 							<v-pagination v-model="page" :length="4" prev-icon="mdi-menu-left" next-icon="mdi-menu-right"></v-pagination>
-						</v-tab-item>
-					</v-tabs-items>
+						</div>
+					</v-row>
 				</v-col>
 
 				<!--     지도 column     -->
@@ -31,26 +71,15 @@
 </template>
 <script>
 import { throttle } from 'lodash'
-import { list, Heading4 } from '@/components'
-import listJson from '../dummy/list'
+import { Heading4 } from '@/components'
+import list from '@/dummy/list.json'
 export default {
 	name: 'Main',
-	components: { list, Heading4 },
-	data: () => ({
-		filterBtnGroup: ['유연한 환불 정책', '숙소 유형', '요금', '즉시 예약', '필터 추가하기'],
-		map: null,
-		mapPosition: false,
-		infoWindow: '',
-		pos: {},
-		tabs: [{ title: '찾기' }, { title: '내가 간 식장들' }],
-		tab: null,
-		items: ['매장찾기', '내가 간 식장들'],
-		listPage: 1,
-		itemPerPage: 3,
-		markers: [],
-	}),
+	components: { Heading4 },
 	mounted() {
+		console.log(list)
 		this.infoWindow = new window.google.maps.InfoWindow()
+		// Try HTML5 geolocation.
 		navigator.geolocation.getCurrentPosition(position => {
 			const pos = {
 				lat: position.coords.latitude,
@@ -69,26 +98,76 @@ export default {
 			},
 			zoom: 13,
 		})
-		let index = this.itemPerPage * (this.page - 1)
-		for (let i = index; i < this.itemPerPage * this.page; i++) {
-			// console.log(listJson[i])
+
+		// this.$store.state.airBnbLists.map(item => {
+		list.forEach(element => {
 			const marker = new window.google.maps.Marker({
 				map: this.map,
 				position: {
-					lat: listJson[i].lat,
-					lng: listJson[i].lng,
+					lat: element.lat,
+					lng: element.lng,
 				},
 			})
-			this.markers.push(marker)
-		}
+			this.attachSecretMessage(marker, element)
+		})
 
+		// 	this.attachSecretMessage(marker, item)
+		// 	// marker.addListener('click', data => {
+		// 	// 	console.log(data)
+		// 	// 	this.map.setCenter(marker.getPosition());
+		// 	// })
+		// })
+
+		// // 서치 데이터 페칭
+		// this.$store
+		// 	.dispatch('getSearchForm', this.$route.query)
+		// 	.then(res => {
+		// 		let { nhits, records } = res.data
+		// 		this.$store.state.totalLists = nhits
+		// 		this.$store.state.airBnbLists = records
+
+		// 		// 구글 맵 초기화
+		// 		this.map = new window.google.maps.Map(document.getElementById('map'), {
+		// 			center: {
+		// 				lat: this.$store.state.airBnbLists[0].fields.geolocation[0],
+		// 				lng: this.$store.state.airBnbLists[0].fields.geolocation[1],
+		// 			},
+		// 			zoom: 13,
+		// 		})
+		// 		console.log(this.$store.state.airBnbLists)
+		// 		// 마커
+		// 		this.$store.state.airBnbLists.map(item => {
+		// 			const marker = new window.google.maps.Marker({
+		// 				map: this.map,
+		// 				position: {
+		// 					lat: item.fields.geolocation[0],
+		// 					lng: item.fields.geolocation[1],
+		// 				},
+		// 			})
+		// 			this.attachSecretMessage(marker, item)
+		// 			// marker.addListener('click', data => {
+		// 			// 	console.log(data)
+		// 			// 	this.map.setCenter(marker.getPosition());
+		// 			// })
+		// 		})
+		// 	})
+		// 	.catch(err => {
+		// 		console.log(err)
+		// 	})
+		// 스크롤 이벤트 등록
 		window.addEventListener('scroll', throttle(this.onScroll, 100))
 	},
 	updated() {},
 	destroyed() {
 		window.removeEventListener('scroll', this.onScroll)
 	},
-
+	data: () => ({
+		filterBtnGroup: ['유연한 환불 정책', '숙소 유형', '요금', '즉시 예약', '필터 추가하기'],
+		map: null,
+		mapPosition: false,
+		infoWindow: '',
+		pos: {},
+	}),
 	computed: {
 		page: {
 			get() {
@@ -116,26 +195,6 @@ export default {
 		},
 		onScroll() {
 			window.pageYOffset < 4400 ? (this.mapPosition = false) : (this.mapPosition = true)
-		},
-	},
-	watch: {
-		page() {
-			for (let i = 0; i < this.markers.length; i++) {
-				this.markers[i].setMap(null)
-			}
-			console.log(this.page)
-			let index = this.itemPerPage * (this.page - 1)
-			for (let i = index; i < this.itemPerPage * this.page; i++) {
-				// console.log(listJson[i])
-				const marker = new window.google.maps.Marker({
-					map: this.map,
-					position: {
-						lat: listJson[i].lat,
-						lng: listJson[i].lng,
-					},
-				})
-				this.markers.push(marker)
-			}
 		},
 	},
 }
